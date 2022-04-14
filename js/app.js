038356e185ae2d5d415db9f5cb7bc262429d682f
Salko <64722632+sarajlija@users.noreply.github.com>
2022-04-14 15:33:43 +0200

    up

--- js/app.js
+++ js/app.js
@@ -28,6 +28,33 @@ const crew = [
         }
     ]
 
+    const technology = [
+        {
+            name: "Launch vehicle",
+            images: {
+                portrait: "./assets/technology/image-launch-vehicle-portrait.jpg",
+                landscape: "./assets/technology/image-launch-vehicle-landscape.jpg"
+            },
+            description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+        },
+        {
+            name: "Spaceport",
+            images: {
+                portrait: "./assets/technology/image-spaceport-portrait.jpg",
+                landscape: "./assets/technology/image-spaceport-landscape.jpg"
+            },
+            description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+        },
+        {
+            name: "Space capsule",
+            images: {
+                portrait: "./assets/technology/image-space-capsule-portrait.jpg",
+                landscape: "./assets/technology/image-space-capsule-landscape.jpg"
+            },
+            description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+        }
+    ]
+
 let buttons = document.querySelectorAll('.btn');
 
 //console.log(buttons.length)
@@ -51,11 +78,44 @@ buttons.forEach((button,i) => {
     });
 });
 
+//TECHNOLOGY
 
-    /*
-     
-            
-          
-            
-                
-     */     
+
+let buttonsTech = document.querySelectorAll('.btn-tech');
+
+//console.log(buttons.length)
+buttonsTech.forEach((button, i) => {
+    button.addEventListener('click', function (e) {
+        var result = e.currentTarget.parentElement.previousElementSibling
+        
+        result.innerHTML = `
+        <div class="technology__title">
+        <p class="technology__p"><span>03</span>SPACE LAUNCH 101</p>
+        <h3>THE TERMINOLOGY...</h3>
+        <h2>${technology[i].name}</h2>
+        <p>${technology[i].description}</p>
+      </div>
+      <div class="technology__img"><img src=${technology[i].images.portrait} alt=${technology[i].name}>
+        </div>`
+        buttonsTech.forEach(btns => btns.classList.remove('active'));
+        this.classList.add('active');
+        // console.log(buttons)
+         console.log(i)
+    });
+});
+
+window.addEventListener("resize", function () {
+  
+    if (window.matchMedia("(min-width: 500px)").matches) {
+       
+        console.log("Screen width is at least 500px")
+    } else {
+        
+        console.log("Screen less than 500px")
+    }
+})
+
+
+
+
+    
